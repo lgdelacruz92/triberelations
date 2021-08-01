4778f6e2b7b1b7878236eafc34958d0341610401
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-08-01 15:46:36 -0400

    listing inquiries

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
+require('ansicolor').nice;
 const app = express();
+const users = require('./src/endpoints/users');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const inquiries = [];
+const session = {};
+const FAILED_TO_LOGIN = 3;
+const UNAUTHENTICATED = 4;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.post('/sms', (req, res) => {
+  console.log(`${JSON.stringify(req.body)}`.red);
+  inquiries.push(req.body);
+  res.send({ code: 200 });
+});
 
-  // Start our TwiML response.
-  const twiml = new MessagingResponse();
+app.get('/inquiries', (req, res) => {
+  if (req.body.token && req.body.token === session.hash) {
+    res.send({ inquiries });
+  } else {
+    res.send({ code: UNAUTHENTICATED, message: 'Unauthenticated: Please login.'});
+  }
+});
 
-  // Add a text message.
-  const msg = twiml.message('Check out this sweet owl!');
-
-  // Add a picture message.
-  msg.media('https://demo.twilio.com/owl.png');
-
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+// Users
+app.get('/users', users.users);
+app.post('/signup', users.signup);
+app.post('/login', (req, res) => {
+  const result = users.login(req, res);
+  if (result.err === false) {
+    session.hash = result.token;
+    res.send({ code: 200, token:  result.token});
+  } else {
+    res.send({ code: FAILED_TO_LOGIN, 'message': 'Error logging in.' });
+  }
 });
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+module.exports = app;
