8e21d68d147307ba636adfc8d937b762867bd108
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-08-03 15:54:13 -0400

    Added cloud functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,18 @@
+/* eslint-disable quote-props */
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: ['eslint:recommended', 'google'],
+    rules: {
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        'object-curly-spacing': ['error', 'always'],
+    },
+    parserOptions: {
+        requireConfigFile: false,
+    },
+    parser: '@babel/eslint-parser',
 };
